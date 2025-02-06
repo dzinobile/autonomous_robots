4f8131b7c2e973b61a4a228f62ee52a7f40392f6
vboxuser <vboxuser@thinkpad.myguest.virtualbox.org>
2025-02-06 12:11:04 -0500

    commit

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -3,6 +3,7 @@ import numpy as np
 import math
 import statistics
 
+#open file and load into lists
 data = open("imudata.txt", "r")
 pitch_angles = []
 time_stamps = []
@@ -11,9 +12,11 @@ for line in data:
     pitch_angles.append(int(split_line[4]))
     time_stamps.append(split_line[1])
 
-n = len(pitch_angles)
-x_array = np.arange(n)
 
+x_array = np.arange(len(pitch_angles)) #create array of x values
+
+#calculate the time increment of each x value based on averaging samples per second
+#not a requirement, but done for accurate labeling of graph
 values_per_second = []
 unique_time_values = list(set(time_stamps))
 for utv in unique_time_values:
@@ -21,15 +24,16 @@ for utv in unique_time_values:
 
 values_per_second.sort(reverse=True)
 values_per_second.pop()
-values_per_second.pop()
+values_per_second.pop()#remove lowest 2, since first and last timestamp would not be a full second
 average_values_per_second = int(sum(values_per_second) / len(values_per_second))
 sample_rate = 1 / average_values_per_second
 
-
+#create a centered moving average function 
 def moving_average_function(window_size):
     moving_average = []
     x_average = []
 
+    #centered moving average for even window sizes
     if window_size % 2 == 0:
         q = int(window_size / 2)
         s = q - 1
@@ -37,24 +41,27 @@ def moving_average_function(window_size):
         for i in range(s, f):
             b = i - (q - 1)
             e = b + (window_size - 1)
-            window = pitch_angles[b:e]
+            window = pitch_angles[b:e+1]
             moving_average.append(sum(window) / window_size)
             x_average.append(x_array[i])
+            
+    #centered moving average for odd window sizes
     else:
         s = math.floor(window_size / 2)
         f = len(pitch_angles) - s
         for i in range(s, f):
             b = i - s
             e = b + (window_size - 1)
-            window = pitch_angles[b:e]
+            window = pitch_angles[b:e+1]
             moving_average.append(sum(window) / window_size)
             x_average.append(x_array[i])
 
+    #calculate mean and std dev
     mean = statistics.mean(moving_average)
     stdev = statistics.stdev(moving_average)
-    return x_average, moving_average, mean, stdev
+    return x_average, moving_average, mean, stdev #return x array for moving averages, list of moving averages, mean, and std dev
 
-
+#create plotting function
 def plot(w_size):
     x, y, m, std = moving_average_function(w_size)
     plt.plot(x_array, pitch_angles, "r", x, y, "b")
@@ -81,11 +88,12 @@ def plot(w_size):
         bbox=dict(boxstyle="round", facecolor="lightgrey", alpha=0.5),
     )
     plt.show()
+    
+
+#plot data with different window sizes
+w_size_list = [2, 4, 8, 16, 64, 128]
+
+for i in w_size_list:
+    plot(i)
 
 
-plot(2)
-plot(4)
-plot(8)
-plot(16)
-plot(64)
-plot(128)
